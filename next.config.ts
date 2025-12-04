0b1d7dc0110cb4e09a667cb82b4611b220fc03c3
shisuiyou <1651037998@qq.com>
2025-12-04 23:25:49 +0800

    .

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  transpilePackages: [  "@ant-design", "antd", "rc-util", "rc-pagination", "rc-picker", "rc-input" ],
 };
 
 export default nextConfig;
